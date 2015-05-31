7a20ee80249e948a9d9f837161ff61efcf4444d9
Hou Tao <hotforest@gmail.com>
2015-05-31 12:48:00 +0800

    add wireshark lua script for onvif replay rtp header extension

---
+++ rtp_ext_onvif_replay.lua
@@ -0,0 +1,42 @@
+--[[
+ONVIF Streaming Specification: 6.2 RTP header extension
+ 0xABAC length=3
+ NTP timestamp
+ NTP timestamp
+ C E D mbz CSeq padding
+--]]
+
+local onvif_replay_proto = Proto("onvif", "ONVIF Replay")
+
+-- ProtoField.new(name, abbr, type, [voidstring], [base], [mask], [descr])
+-- base: one of BASE_, or field bit-width if FT_BOOLEAN and non-zero bitmask (_header_field_info)
+local ntp_sec = ProtoField.new("NTP Second", "onvif.sec", ftypes.UINT32, nil, BASE.DEC_HEX)
+local ntp_nsec = ProtoField.new("NTP Nanosecond", "onvif.nsec", ftypes.UINT32, nil, BASE.DEC_HEX)
+local ntp_clean = ProtoField.new("Clean", "onvif.c", ftypes.BOOLEAN, nil, 8, 0x80)
+local ntp_end = ProtoField.new("End", "onvif.e", ftypes.BOOLEAN, nil, 8, 0x40)
+local ntp_discon = ProtoField.new("Discon", "onvif.d", ftypes.BOOLEAN, nil, 8, 0x20)
+local ntp_mbz = ProtoField.new("MBZ", "onvif.mbz", ftypes.UINT8, nil, BASE.DEC, 0x1F)
+local ntp_seq = ProtoField.new("Seq", "onvif.seq", ftypes.UINT8)
+local ntp_pad = ProtoField.new("Pad", "onvif.pad", ftypes.UINT16)
+
+function onvif_replay_proto.dissector(buf, pinfo, tree)
+	if buf:len() ~= 12 then return end
+
+	local subtree = tree:add(onvif_replay_proto, buf())
+
+	subtree:add(ntp_sec, buf(0,4))
+	subtree:add(ntp_nsec, buf(4,4))
+
+	subtree:add(ntp_clean, buf(8,1))
+	subtree:add(ntp_end, buf(8,1))
+	subtree:add(ntp_discon, buf(8,1))
+	subtree:add(ntp_mbz, buf(8,1))
+
+	subtree:add(ntp_seq, buf(9,1))
+
+	subtree:add(ntp_pad, buf(10,2))
+end
+
+local rtp_hdr_ext_table = DissectorTable.get("rtp.hdr_ext")
+rtp_hdr_ext_table:add(0xABAC, onvif_replay_proto)
+
